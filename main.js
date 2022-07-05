d00fb91e5986e6481667e9c8380d284808ac1efb
Maria de Lourdes Carvalho Chaves <95364349+maluchaves91@users.noreply.github.com>
2022-07-05 16:56:06 -0300

    luluca

---
+++ main.js
@@ -0,0 +1,38 @@
+function bodyLoad() {
+    document.getElementById("bt").style.fontSize = "15px";
+    document.getElementById("bt").style.backgroundColor="orange";
+    document.getElementById("bt").style.borderRadius="20px";
+    document.getElementById("bt").style.color="white";
+    document.getElementById("bt").style.padding="10px";
+    document.getElementById("bt").style.borderStyle="none"
+    
+    document.getElementById("bt2").style.fontSize = "15px";
+    document.getElementById("bt2").style.backgroundColor="orange";
+    document.getElementById("bt2").style.borderRadius="20px";
+    document.getElementById("bt2").style.color="white";
+    document.getElementById("bt2").style.padding="10px";
+    document.getElementById("bt2").style.borderStyle="none"
+    
+    }
+    
+    function voltarTema () {
+    document.getElementById("body").style.backgroundColor="pink";
+    document.getElementById("titulo").style.backgroundColor="pink";
+    }
+    
+    function mudarTema()
+    {
+        document.getElementById("body").style.backgroundColor="purple";
+        document.getElementById("titulo").style.backgroundColor="yellow";
+       }
+    
+    function mouseCima()
+    {
+        document.getElementById("img_desenho").src ="https://yt3.ggpht.com/UNVCrM9WSTscqZS_i2G_FNXitVFcdCkv1kW-PEvJcObR_-t7Zb44BIJRhgwuj6jLXrMv_2G1=s900-c-k-c0x00ffffff-no-rj";
+    }
+    
+    function mouseFora()
+    {
+        document.getElementById("img_desenho").src ="https://acordeemny.com.br/wp-content/uploads/2020/01/numero-da-luluca.jpg";
+    }
+    
